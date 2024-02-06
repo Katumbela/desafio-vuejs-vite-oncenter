04aca3a6a8aa72f1546094033aa640f2e6dacd3b
João Afonso Katombela <88532376+Katumbela@users.noreply.github.com>
2024-02-06 12:22:26 +0100

    Update vite.config.js

--- web/vite.config.js
+++ web/vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   plugins: [vue()],
   build: {
     rollupOptions: {
-      external: ['chart.js/auto', 'axios'],
+      external: ['chart.js/auto', 'axios', 'numeral'],
     }
   }
 });
